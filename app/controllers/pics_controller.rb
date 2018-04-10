58f1854d78d52a132eec95aca8b512eed5011b32
Jeremy Edwards <edwardsbjeremy@gmail.com>
2018-04-10 12:47:06 -0500

    +Images for posts

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,22 @@
 class PicsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @pics = Pic.all.order('created_at DESC')
+  end
+
+  def new
+    @pic = Pic.new
+  end
+
+  #creates post, and if successfull, goes to post
+  def create
+    @pic = Pic.new(pic_params);
+
+    if @pic.save
+      redirect_to @pic
+    else
+      render 'new'
+    end
   end
 end
