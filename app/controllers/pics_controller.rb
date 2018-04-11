0ba6e2fb692d9cb40e90ba7b1bfa5d8ff97f0666
Jeremy Edwards <edwardsbjeremy@gmail.com>
2018-04-11 15:59:07 -0500

    +Delete & +Edit functions and buttons

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,6 @@
 class PicsController < ApplicationController
   #before_action :authenticate_user!, except: [:index, :show]
+  #before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
     @pics = Pic.all.order('created_at DESC')
@@ -9,17 +10,42 @@ class PicsController < ApplicationController
     @pic = Pic.new
   end
 
+  def show
+  end
+
   #creates post, and if successfull, goes to post
   def create
     @pic = Pic.new(pic_params)
 
     if @pic.save
       redirect_to @pic
+    else
+      render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+  
   private
     def pic_params
       params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+      @pic = Pic.find(params[:id])
     end
 end
